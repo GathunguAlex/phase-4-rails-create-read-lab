41255cc18e52610a40e462bb99a2c7f9083dc201
GathunguAlex <alex.gathungu@student.moringaschool.com>
2022-12-01 01:52:55 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :plants, only: [:index, :show, :create]
+
+   get "/plant", to:"plant#index"
+    get "/plant/id", to:"plant#show"
+    post "/plant", to:"plant#create"
 end
